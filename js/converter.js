222d3cb458710256155cd2437f594dd34383d03b
Roger Bernardo <rogerbernardo007@gmail.com>
2020-04-02 19:23:18 -0300

    Feature: Layout da calculadora

--- js/converter.js
+++ js/converter.js
@@ -196,3 +196,101 @@ function operations(number1, base1, number2, base2, operator) {
             return;
     }
 }
+
+let togglerCalc = 0;
+function changeNumberToLetter() {
+    let number_0 = document.getElementById('calc_number_0');
+    let number_1 = document.getElementById('calc_number_1');
+    let number_2 = document.getElementById('calc_number_2');
+    let number_3 = document.getElementById('calc_number_3');
+    let number_4 = document.getElementById('calc_number_4');
+    let number_5 = document.getElementById('calc_number_5');
+    let number_6 = document.getElementById('calc_number_6');
+    let number_7 = document.getElementById('calc_number_7');
+    let number_8 = document.getElementById('calc_number_8');
+    let number_9 = document.getElementById('calc_number_9');
+
+
+    if (togglerCalc % 2 == 0) {
+        number_0.innerHTML = 'A';
+        number_0.setAttribute('name', 'A');
+
+        number_1.innerHTML = 'B';
+        number_1.setAttribute('name', 'B');
+
+
+        number_2.innerHTML = 'C';
+        number_2.setAttribute('name', 'C');
+
+        number_3.innerHTML = 'D';
+        number_3.setAttribute('name', 'D');
+
+        number_4.innerHTML = 'E';
+        number_4.setAttribute('name', 'E');
+
+        number_5.innerHTML = 'F';
+        number_5.setAttribute('name', 'F');
+
+        number_6.innerHTML = 'G';
+        number_6.setAttribute('name', 'G');
+
+        number_7.innerHTML = 'H';
+        number_7.setAttribute('name', 'H');
+
+        number_8.innerHTML = 'I';
+        number_8.setAttribute('name', 'I');
+
+        number_9.innerHTML = 'J';
+        number_9.setAttribute('name', 'J');
+    }
+
+    else {
+        number_0.innerHTML = '0';
+        number_0.setAttribute('name', '0');
+
+        number_1.innerHTML = '1';
+        number_1.setAttribute('name', '1');
+
+        number_2.innerHTML = '2';
+        number_2.setAttribute('name', '2');
+
+        number_3.innerHTML = '3';
+        number_3.setAttribute('name', '3');
+
+        number_4.innerHTML = '4';
+        number_4.setAttribute('name', '4');
+
+        number_5.innerHTML = '5';
+        number_5.setAttribute('name', '5');
+
+        number_6.innerHTML = '6';
+        number_6.setAttribute('name', '6');
+
+        number_7.innerHTML = '7';
+        number_7.setAttribute('name', '7');
+
+        number_8.innerHTML = '8';
+        number_8.setAttribute('name', '8');
+
+        number_9.innerHTML = '9';
+        number_9.setAttribute('name', '9');
+    }
+
+    togglerCalc++;
+}
+
+function addToVisor(particle) {
+    document.getElementById('calc_visor_expressions').innerHTML += particle;
+    console.log(particle);
+}
+
+function cleanVisor() {
+    document.getElementById('calc_visor_expressions').innerHTML = '';
+    document.getElementById('calc_visor_result').innerHTML = '';
+}
+
+function removeLastParticle() {
+    let expression = document.getElementById('calc_visor_expressions').innerHTML;
+    cleanExpression = expression.substring(0, (expression.length - 1));
+    document.getElementById('calc_visor_expressions').innerHTML = cleanExpression;
+}
